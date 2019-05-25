b232f6227ad397f756ef21f868628c91a0a658fa
Marcus Hilaire <marcushilaire@gmail.com>
2019-05-25 12:11:06 -0400

    crafting axios get url

---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+export default {
+  callProxy: function(route, x, y) {
+    return axios.get(
+      `http://kevin-45c9c78a-eval-test.apigee.net/calculator-master/${route}?x=${x}&y=${y}`
+    );
+  }
+};
