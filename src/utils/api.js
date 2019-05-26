340328f84ed5728d4d24980f43cd96b963fc2822
Marcus Hilaire <marcushilaire@gmail.com>
2019-05-26 15:31:10 -0400

    changed to https

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export default {
   callProxy: function(route, x, y) {
     return axios.get(
-      `http://kevin-45c9c78a-eval-test.apigee.net/calculator-master/${route}?x=${x}&y=${y}`
+      `https://kevin-45c9c78a-eval-test.apigee.net/calculator-master/${route}?x=${x}&y=${y}`
     );
   }
 };
